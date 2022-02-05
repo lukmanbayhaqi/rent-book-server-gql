941c0f9c9e2702f1495f0e4fdbfb27bc3fe43887
Lukman Bayhaqi <lukman@Lukmans-MacBook-Air.local>
2022-02-05 16:17:40 +0700

    try to deploy server

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require("apollo-server-express");
-const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
@@ -15,7 +18,10 @@ async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
     context: ({ req }) => {
       return req.headers.authorization ?? "";
     },
